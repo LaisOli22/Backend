6e3437eb811675688071e53a18df38ad6de3a094
LaisOli22 <laislsolalis@gmail.com>
2024-01-17 17:41:21 -0300

    Conexão com o Frontend

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,38 @@
 import fastify from "fastify";
 import { prisma } from "./lib/prisma";
-
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client';
 const server = fastify();
+const prisma = new PrismaClient();
+
+server.register(cors), {
+  origin: 'http://localhost:3000',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}; 
 
 server.get("/products", async (req, reply) => {
-    const allProducts = await prisma.produtos.findMany();
-    reply.send({ products: allProducts });
+    const products = await prisma.produtos.findMany();
+    console.log(products);
+    reply.send(products);
 });
 
-server.get("/products/:productname/:price", async (req, reply) => {
-    const { productname, price } = req.params;
-  await prisma.produtos.create({
-    data: {
-      nome: productname,
-      preco: parseFloat(price),
-    },
-  });
-  reply.send({ message: "Product added successfully" });
+server.post("/products", async (req, reply) => {
+  const { name } = req.body;
+
+  try {
+    const newProduct = await prisma.produtos.create({
+      data: {
+        nome: name,
+        preco: 0, 
+      },
+    });
+  reply.send({newProduct});
+  
+} catch (error) {
+  console.error('Erro ao adicionar produto:', error);
+  reply.status(500).send({ error: 'Internal Server Error' });
+}
 });
 
 server.listen({ port: 3001 }, (err, address) => {
